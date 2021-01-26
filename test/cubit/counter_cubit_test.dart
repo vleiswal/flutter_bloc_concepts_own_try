f4d22f36b49b47f2931c316f50ee89d358743d7a
Vleis Walker <vleiswal@gmail.com>
2021-01-26 04:41:27 +0200

    test complete

--- test/cubit/counter_cubit_test.dart
+++ test/cubit/counter_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_bloc_concepts_own_try/cubit/counter_cubit.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -16,5 +17,19 @@ void main() {
         () {
       expect(counterCubit.state, CounterState(counterValue: 0));
     });
+
+    blocTest(
+      "the cubit should emit a CounterState(counterValue:1, wasIncremented:true) when cubit.increment() function is called",
+      build: () => counterCubit,
+      act: (cubit) => cubit.increment(),
+      expect: [CounterState(counterValue: 1, wasIncremented: true)],
+    );
+
+    blocTest(
+      "the cubit should emit a CounterState(counterValue:-1, wasIncremented:false) when cubit.increment() function is called",
+      build: () => counterCubit,
+      act: (cubit) => cubit.decrement(),
+      expect: [CounterState(counterValue: -1, wasIncremented: false)],
+    );
   });
 }
